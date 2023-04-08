8c7eb1bc76fc5ba7c6e95f549e1c1574c87ce2d4
chendelin <chendelin@rantron.com>
2023-04-08 23:09:42 +0800

    init

---
+++ src/main/java/com/delin/lazy/core/Filter.java
@@ -0,0 +1,13 @@
+package com.delin.lazy.core;
+
+/**
+ * Filter
+ *
+ * @author: delingChen
+ * @version: 1.0-SN
+ * @date: 2023/4/8 12:26 星期六
+ */
+public interface Filter {
+
+    void doFilter() throws Exception;
+}
